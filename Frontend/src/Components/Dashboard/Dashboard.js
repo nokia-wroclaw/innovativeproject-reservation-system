34a82cb6279920bffe3d219e4e1dd00d477193b5
maciejjaskolski <226242@student.pwr.wroc.pl>
2018-05-07 10:52:46 +0200

    localhost fix, dashboard routes fix

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -3,10 +3,21 @@ import AuthService from '../AuthService';
 import withAuth from '../withAuth';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 
-import {DASHBOARD_ROUTES} from '../../routes'
+//import {DASHBOARD_ROUTES} from '../../routes'
 import './dashboard.css'
 
 const Auth = new AuthService();
+
+const DASHBOARD_ROUTES = [{
+  title: 'My reservations',
+  to: '/myreservations'
+},{
+  title: 'My profile',
+  to: '/profile'
+}, {
+  title: 'Edit profile',
+  to: '/profile/edit'
+}]
 
 
 class Profile extends Component {
